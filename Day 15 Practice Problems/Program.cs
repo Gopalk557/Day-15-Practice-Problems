3cc1be1e21487a803df54942a74dbae236791ae6
GOPAL THAKUR <gopalk557@gmail.com>
2022-03-03 00:19:57 +0530

    Welcome to Day 15 Practice problems

---
+++ Day 15 Practice Problems/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_15_Practice_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to practice Day 15");
+            Console.ReadKey();
+        }
+    }
+}
