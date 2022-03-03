4c088e71e09ac7748907f215d5655d1373a110df
GOPAL THAKUR <gopalk557@gmail.com>
2022-03-04 01:53:39 +0530

    [Gopal]Add:Find frequency of word

---
+++ Day 15 Practice Problems/MyMapNode.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_15_Practice_Problems
+{
+    public class MyMapNode<K, V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> items in linkedList)
+            {
+                if (items.Key.Equals(key))
+                {
+                    return items.Value;
+                }
+
+            }
+            return default(V);
+        }
+
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> items = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedList.AddLast(items);
+        }
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            bool itemsFound = false;
+            KeyValue<K, V> foundItems = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> items in linkedList)
+            {
+                if (items.Key.Equals(key))
+                {
+                    itemsFound = true;
+                    foundItems = items;
+                }
+            }
+            if (itemsFound)
+            {
+                linkedList.Remove(foundItems);
+            }
+
+        }
+
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+
+        public struct KeyValue<K, V>
+        {
+            public K Key { get; set; }
+            public V Value { get; set; }
+        }
+    }
+}
--- Day 15 Practice Problems/Program.cs
+++ Day 15 Practice Problems/Program.cs
@@ -11,6 +11,19 @@ namespace Day_15_Practice_Problems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to practice Day 15");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value: " + hash5);
+
+            string hash3 = hash.Get("3");
+            Console.WriteLine("2th index value: " + hash3);
             Console.ReadKey();
         }
     }
